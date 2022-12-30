d8356e6f3146eb81907fd77a0c7079d84bf87377
thomas084 <56292903+thomas084@users.noreply.github.com>
2022-12-30 22:27:46 +0530

    initial push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import './App.css'
+import { useEffect, useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [newName,setnewName]=useState()
+  const [newEmail,setnewEmail]=useState()
+  useEffect(()=>{
+    fetch();
+  },[])
+  async function fetch() {
+    const response=await axios.get("https://randomuser.me/api");
+    if(response){
+      const data=response;
+      const title=data.data.results[0].name.title
+      const firstName=data.data.results[0].name.first
+      const lastname=data.data.results[0].name.last
+      var name=title + ". " + firstName +" " + lastname
+      var email=data.data.results[0].email
+      localStorage.setItem("name",name);
+      localStorage.setItem("email",email);
+      setnewName(name)
+      setnewEmail(email)
+    }
+  }
+  
+    return (
+      <div><div className="App-header">FullName: {localStorage.getItem("name")} <br/>Email: {localStorage.getItem("email")}
+        </div>
+        <br/>
+        <div className="App"><button onClick={fetch}>Refresh</button></div>
+      </div>
+    );
 }
 
 export default App;
