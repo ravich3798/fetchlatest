cdc583193d3ea160be47eb8d514f656f624842e7
thomas084 <56292903+thomas084@users.noreply.github.com>
2022-12-31 13:08:03 +0530

    v2

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ function App() {
   }
   
     return (
-      <div><div className="App-header">FullName: {localStorage.getItem("name")} <br/>Email: {localStorage.getItem("email")}
+      <div><div className="App-header">FullName: newName <br/>Email: newEmail
         </div>
         <br/>
         <div className="App"><button onClick={fetch}>Refresh</button></div>
